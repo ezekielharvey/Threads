8a01d3bce096ed5f9d0138372602dbe935b23150
ezekielharvey <zekeharvey@outlook.com>
2023-09-24 01:49:46 -0400

    Thread Card completed

---
+++ app/(root)/thread/[id]/page.tsx
@@ -0,0 +1,8 @@
+// const Page = ({ params }: { params: { id: string }}) => {
+//     <section className="relative">
+//         <div>
+//             <div>
+//     </section>
+// }
+
+// export default Page;
