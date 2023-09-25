d6ac8b9baa95c69ee1e3e418d82885ced7a98d7a
ezekielharvey <zekeharvey@outlook.com>
2023-09-25 11:02:46 -0400

    Profile Page completed Part 1

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -61,28 +61,28 @@ function PostThread({ userId }: { userId: string }) {
             onSubmit={form.handleSubmit(onSubmit)} 
             className="mt-10 flex flex-col justify-start gap-10"
             >
-                <FormField
-            control={form.control}
-            name="thread"
-            render={({ field }) => (
-              <FormItem className="flex flex-col gap-3 w-full">
-                <FormLabel className="text-base-semibold text-light-2">
-                  Content
-                </FormLabel>
-                <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
-                  <Textarea
-                      rows={15}
-                      {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+              <FormField
+              control={form.control}
+              name="thread"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-3 w-full">
+                  <FormLabel className="text-base-semibold text-light-2">
+                    Content
+                  </FormLabel>
+                  <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
+                    <Textarea
+                        rows={15}
+                        {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <Button type="submit" className="bg-primary-500">
-            Post Thread
-          </Button>
+              <Button type="submit" className="bg-primary-500">
+                Post Thread
+              </Button>
             </form>
         </Form>
     )
